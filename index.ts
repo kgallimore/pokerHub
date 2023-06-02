0ae8fd167c56283c413f91fca67a6485f14bed09
kgallimore <72752967+kgallimore@users.noreply.github.com>
2023-06-01 23:16:42 -0400

    Remove Test

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import { SerialPort, ReadlineParser } from "serialport";
-// Test Commit
 const serialPorts: SerialPort[] = [];
 let currentState: { [key: string]: { [key: string]: string } } = {};
 SerialPort.list().then((ports) => {
