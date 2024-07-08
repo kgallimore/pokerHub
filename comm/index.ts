e8e0a054cfbf35e75f4b7180ef93cc29c6287619
kgallimore <72752967+kgallimore@users.noreply.github.com>
2024-07-08 00:26:06 -0400

    Update comm

--- comm/index.ts
+++ comm/index.ts
@@ -1,12 +1,41 @@
-import { SerialPort, ReadlineParser } from "serialport";
-import { CommPortUpdate, SensorUpdate } from "./../svelte/src/lib/comm";
+import { SerialPort } from "serialport";
+import { ReadlineParser } from '@serialport/parser-readline';
 import { usb } from "usb";
+import PocketBase from 'pocketbase';
+import type { CommPortsResponse, SensorsResponse, TypedPocketBase } from "./../svelte/src/lib/pocketbase-types"
+import { createInterface } from "readline";
+import eventsource from 'eventsource';
+// @ts-expect-error Polyfill
+global.EventSource = eventsource;
 import "dotenv/config";
 
+const adminEmail = process.env.PB_ADMIN_EMAIL;
+if(!adminEmail) throw new Error("No admin email provided");
+const adminPassword = process.env.PB_ADMIN_PASSWORD;
+if(!adminPassword) throw new Error("No admin password provided");
+
+const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase;
+await pb.admins.authWithPassword(adminEmail, process.env.PB_ADMIN_PASSWORD as string);
+const commPorts = await pb.collection("CommPorts").getFullList<CommPortsResponse>();
+const cardLookup = await pb.collection("Cards").getFullList();
+await pb.collection("CommPorts").subscribe<CommPortsResponse>("*", async (data) => {
+  switch (data.action) {
+    case "create":
+      commPorts.push(data.record);
+      break;
+    case "update":
+      const index = commPorts.findIndex((port) => port.id === data.record.id);
+      commPorts[index] = data.record;
+      break;
+    case "delete":
+      commPorts.splice(commPorts.findIndex((port) => port.id === data.record.id), 1);
+      break;
+  }
+});
 const serialPorts: {
   [port: string]: { port: SerialPort; numOfSensors: number; timeout: NodeJS.Timeout };
 } = {};
-let currentState: {
+const currentState: {
   [port: string]: { [sensor: string]: { sendRemoval?: NodeJS.Timeout; value: string }[] };
 } = {};
 usb.on("attach", (device) => {
@@ -30,7 +59,7 @@ function attachSerialPorts() {
           continue;
         }
         console.log("Found Arduino, attaching: " + port.path);
-        const newPort = new SerialPort({ path: port.path, baudRate: 9600 });
+        const newPort = new SerialPort({ path: port.path, baudRate: 115200 });
         newPort.pipe(new ReadlineParser());
         serialPorts[portNum] = {
           port: newPort,
@@ -65,8 +94,8 @@ function attachSerialPorts() {
         newPort.on("error", (err) => {
           console.log(err);
         });
-        newPort.on("close", () => {
-          postCommPortUpdate({ commPort: parseInt(portNum), connected: false });
+        newPort.on("close", async () => {
+          await postCommPortUpdate({ commPort: parseInt(portNum), connected: false });
           console.log(`Port ${port.path} closed`);
           delete serialPorts[portNum];
         });
@@ -77,7 +106,7 @@ function attachSerialPorts() {
 attachSerialPorts();
 
 if (process.platform === "win32") {
-  var rl = require("readline").createInterface({
+  var rl = createInterface({
     input: process.stdin,
     output: process.stdout,
   });
@@ -96,7 +125,6 @@ function parseMessage(message: string, commPort: number) {
   const port = commPort.toString();
   var splitMessage = message.split(":");
   var readerNumber = splitMessage[0].match(/\d+/)?.[0] as string;
-  console.log("Read message: ", splitMessage[1]);
   var cardIds: string[];
   try {
     cardIds = JSON.parse(splitMessage[1]);
@@ -104,6 +132,7 @@ function parseMessage(message: string, commPort: number) {
     console.log("Invalid JSON");
     return;
   }
+  if(Array.isArray(cardIds) === false){ return;}
   if (
     currentState[port][readerNumber] === undefined ||
     currentState[port][readerNumber].length === 0
@@ -135,49 +164,59 @@ function parseMessage(message: string, commPort: number) {
   postSensorUpdate({ commPort, sensor: parseInt(readerNumber), value: cardIds });
 }
 
-async function postSensorUpdate(data: SensorUpdate) {
-  console.log("Posting sensor update");
+async function postSensorUpdate(data: { commPort: number, sensor: number, value: Array<string> }) {
   if (serialPorts[data.commPort.toString()].numOfSensors === 0) return;
-  fetch("http://localhost:5173/api/commPorts/sensors", {
-    method: "POST",
-    mode: "cors",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-      "x-api-key": process.env.API_KEY as string,
-    },
-  })
-    .then(async (response) => {
-      response.ok
-        ? console.log("Posted update")
-        : console.log(
-            "Unable to post " + JSON.stringify(data) + ": " + response.statusText,
-            await response.text()
-          );
-    })
-    .catch(function (err) {
-      console.log("Unable to post -", err);
-    });
+  const existingCommPort = commPorts.find((port) => port.port === data.commPort);
+  if (!existingCommPort) {console.warn("Tried to post sensor update to null port");return};
+
+  let cardIds = data.value.map((card) => {
+    return cardLookup.find((c) => c.uid === card)?.id ?? '';
+  });
+  console.log(data, existingCommPort.sensors, data.sensor, cardIds)
+
+  await pb.collection("Sensors").update(existingCommPort.sensors[data.sensor], {cards: cardIds});
+
 }
 
-async function postCommPortUpdate(data: CommPortUpdate) {
-  console.log("Posting comm port update");
-  if (serialPorts[data.commPort.toString()].numOfSensors === 0) return;
-  fetch("http://localhost:5173/api/commPorts", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-      "x-api-key": process.env.API_KEY as string,
-    },
-    body: JSON.stringify(data),
-  })
-    .then(async (response) => {
-      response.ok
-        ? console.log("Posted update")
-        : console.log("Unable to post: " + response.statusText, await response.text());
-    })
-    .catch(function (err) {
-      console.log("Unable to post -", err);
+async function postCommPortUpdate(data: {
+  commPort: number,
+  connected: boolean,
+  numberOfSensors?: number,
+}) {
+  const existingCommPort = commPorts.find((port) => port.port === data.commPort);
+  if(existingCommPort){
+    if(data.numberOfSensors && existingCommPort.sensors.length !== data.numberOfSensors){
+      const createdSensors: Array<string> = [];
+        for(let i = existingCommPort.sensors.length; i < data.numberOfSensors; i++){
+          createdSensors.push((await pb.collection("Sensors").create({
+            commPort: existingCommPort.id,
+            cards: [],
+          })).id);
+        }
+        await pb.collection("CommPorts").update(existingCommPort.id, {
+          connected: data.connected,
+          sensors: createdSensors
+        });
+    }else{
+      await pb.collection("CommPorts").update(existingCommPort.id, {
+        connected: data.connected,
+      });
+    }
+  }else if(data.numberOfSensors){
+    const newPort = await pb.collection("CommPorts").create({
+      port: data.commPort,
+      connected: data.connected,
+      numberOfSensors: data.numberOfSensors,
     });
+    const createdSensors: Array<string> = [];
+    for(let i = 0; i < data.numberOfSensors; i++){
+      createdSensors.push((await pb.collection("Sensors").create({
+        commPort: newPort.id,
+        cards: [],
+      })).id);
+    }
+    await pb.collection("CommPorts").update(newPort.id, {
+      sensors: createdSensors
+    });
+  }
 }
--- svelte/src/lib/pocketbase-types.ts
+++ svelte/src/lib/pocketbase-types.ts
@@ -39,11 +39,23 @@ export type AuthSystemFields<T = never> = {
 export type CardsRecord = {
 	rank: string
 	suit: string
+	uid: string
 }
 
+export enum CommPortsPositionOptions {
+	"E1" = "1",
+	"E2" = "2",
+	"E3" = "3",
+	"E4" = "4",
+	"E5" = "5",
+	"E6" = "6",
+	"E7" = "7",
+	"E8" = "8",
+}
 export type CommPortsRecord = {
 	connected?: boolean
 	port: number
+	position?: CommPortsPositionOptions
 	sensors?: RecordIdString[]
 }
 
