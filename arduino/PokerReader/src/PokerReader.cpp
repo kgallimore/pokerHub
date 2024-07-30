68286b911071ab93f6768f16d7584c58bd34b5cc
kgallimore <72752967+kgallimore@users.noreply.github.com>
2024-07-29 22:54:45 -0400

    Remove ESP32

--- arduino/PokerReader/src/PokerReader.cpp
+++ arduino/PokerReader/src/PokerReader.cpp
@@ -9,21 +9,11 @@
 #define EXPECTED_NUM_CARDS 2
 #define CARD_READ_TIMEOUT 15
 
-#if defined(ESP32)
-#define MULTI_THREADED true
-TaskHandle_t Task0;
-TaskHandle_t Task1;
-#else
-#define MULTI_THREADED false
-#endif
-
 NfcAdapter *nfc[NUMBER_READERS];
 String tagIds[NUMBER_READERS];
 Sensor *sensors[NUMBER_READERS];
 
 void readNFC(int startingSensor, int endingSensor);
-void readNFCTask(void *param);
-// void readNFCTask1(void *param);
 void connectReader(int index, int pin);
 
 void setup(void)
@@ -47,28 +37,6 @@ void setup(void)
       }
     }
   }
-  if (MULTI_THREADED)
-  {
-    int startEndParms[2] = {0, 1};
-    xTaskCreatePinnedToCore(
-        readNFCTask, /* Function to implement the task */
-        "Task0",     /* Name of the task */
-        10000,       /* Stack size in words */
-        NULL,        /* Task input parameter */
-        0,           /* Priority of the task */
-        &Task0,      /* Task handle. */
-        0);          /* Core where the task should run */
-    startEndParms[0] = 1;
-    startEndParms[1] = NUMBER_READERS;
-    xTaskCreatePinnedToCore(
-        readNFCTask, /* Function to implement the task */
-        "Task1",     /* Name of the task */
-        10000,       /* Stack size in words */
-        NULL,        /* Task input parameter */
-        0,           /* Priority of the task */
-        &Task1,      /* Task handle. */
-        1);          /* Core where the task should run */
-  }
 }
 
 void connectReader(int index, int pin)
@@ -84,8 +52,6 @@ void connectReader(int index, int pin)
 
 void loop()
 {
-  Serial.print("Core: ");
-  Serial.println(xPortGetCoreID());
   if (Serial.available() > 0)
   {
     String incomingString = Serial.readString();
@@ -103,10 +69,7 @@ void loop()
       Serial.println(incomingString);
     }
   }
-  if (!MULTI_THREADED)
-  {
-    readNFC(0, NUMBER_READERS);
-  }
+  readNFC(0, NUMBER_READERS);
 }
 
 void readNFC(int startingSensor, int endingSensor)
@@ -133,29 +96,3 @@ void readNFC(int startingSensor, int endingSensor)
     }
   }
 }
-void readNFCTask(void *param)
-{
-  for (;;)
-  {
-    Serial.print("Read Core 1: ");
-    uint32_t core = xPortGetCoreID();
-    Serial.println(core);
-    if (core == 0)
-    {
-      readNFC(0, 1);
-    }
-    else
-    {
-      readNFC(1, NUMBER_READERS);
-    }
-  }
-}
-// void readNFCTask1(void *param)
-// {
-//   for (;;)
-//   {
-//     Serial.print("Read Core 2: ");
-//     Serial.println(xPortGetCoreID());
-//     readNFC(1, NUMBER_READERS);
-//   }
-// }
